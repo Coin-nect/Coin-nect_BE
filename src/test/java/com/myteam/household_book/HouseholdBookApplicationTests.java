8645c4111d2db58ccbd892121b0de6eb16eced27
ryu yeoni <yeoni0915@naver.com>
2025-01-18 03:40:55 +0900

    Add initial changes in yeon branch

---
+++ src/test/java/com/myteam/household_book/HouseholdBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteam.household_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
