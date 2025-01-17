abe78412bdf58f209f3371762c6d2a04ea13bb3a
ryu yeoni <yeoni0915@naver.com>
2025-01-18 03:40:55 +0900

    Add initial changes in yeon branch

---
+++ src/main/java/com/myteam/household_book/HouseholdBookApplication.java
@@ -0,0 +1,13 @@
+package com.myteam.household_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdBookApplication.class, args);
+	}
+
+}
