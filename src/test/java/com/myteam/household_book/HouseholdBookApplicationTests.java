0b645b48cab4f43f7b32407d395ff21d92d6b629
ryu yeoni <yeoni0915@naver.com>
2025-02-03 00:27:29 +0900

    Resolved conflicts in application.properties and HouseholdBookApplicationTests.java

--- src/test/java/com/myteam/household_book/HouseholdBookApplicationTests.java
+++ src/test/java/com/myteam/household_book/HouseholdBookApplicationTests.java
@@ -1,9 +1,7 @@
 package com.myteam.household_book;
 
-<<<<<<< HEAD
-=======
+
 import org.junit.jupiter.api.Disabled;
->>>>>>> 020f8f467c24a9871608fa526315b4d506afb95c
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,10 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HouseholdBookApplicationTests {
 
 	@Test
-<<<<<<< HEAD
-=======
 	@Disabled
->>>>>>> 020f8f467c24a9871608fa526315b4d506afb95c
+
 	void contextLoads() {
 	}
 
